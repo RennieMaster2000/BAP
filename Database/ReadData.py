c2f665f7a0f9e8549cebadc9403b9d30f0138a3d
RennieMaster2000 <menkveldrenee@gmail.com>
2025-05-27 11:25:53 +0200

    cleanup and dataset access function

--- Database/ReadData.py
+++ Database/ReadData.py
@@ -1,3 +1,9 @@
 import pandas as pd
-list = pd.read_csv('C:\\Users\\Renéee\\Documents\\GitHub\\BAP\\Dataset\\Data Tables\\HDeviceCGM.txt',nrows=30,sep='|',dtype={'ID':'int','glucose':'float'},parse_dates=['time'],usecols=['ID','glucose','time'],header=0,names=['0','ID','1','2','3','time','days','internaltime','4','glucose'])
+import os
+
 print(list)
+
+def getDataPandas(length, start):
+    dirname = os.path.dirname(__file__)
+    filename = os.path.join(dirname, 'CGMData.txt')
+    list = pd.read_csv(filename,skiprows=start,nrows=length,sep='|',dtype={'ID':'int','glucose':'float'},parse_dates=['time'],usecols=['ID','glucose','time'],header=0,names=['0','ID','1','2','3','time','days','internaltime','4','glucose'])
