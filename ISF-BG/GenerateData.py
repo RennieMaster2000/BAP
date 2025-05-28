61d7d24beb9d1ed01edf62875d0724b60ec7d1d9
RennieMaster2000 <menkveldrenee@gmail.com>
2025-05-28 17:01:05 +0200

    Got Data Generation Working

--- ISF-BG/GenerateData.py
+++ ISF-BG/GenerateData.py
@@ -2,12 +2,14 @@ import numpy as np
 import math
 import datetime
 import ReadData
+import matplotlib.pyplot as plt
+import matplotlib.dates as mpld
 
 def Timestep(source,array,length,isfK,bloodK,T,D):
     buffer = np.zeros(length)
     for i in range(length):
         if i==0:
-            buffer[0]=array[0]+T*(D*(array[1]-array[0])+(source*bloodK-array[0]*isfK))
+            buffer[0]=array[0]+T*(D*(array[1]-array[0])+(source*bloodK+array[0]*(isfK-1)))
         elif i==length-1:
             buffer[i]=array[i]+T*D*(array[i-1]-array[i])
         else:
@@ -15,42 +17,55 @@ def Timestep(source,array,length,isfK,bloodK,T,D):
     return buffer
 
 def DetermineBloodISFConstant(P,F,A,Rstarling,sigmapi,Pv,Pa,Kf):
-    '''
     if Rstarling!=0:
-        Jgain=Kf*(0.5*sigmapi^2-sigmapi*Pa+0.5*Pa^2)/(Pv-Pa)
-        Jloss=Kf*(0.5*sigmapi^2-sigmapi*Pv+0.5*Pv^2)/(Pv-Pa)
+        Jgain=Kf*(0.5*sigmapi**2-sigmapi*Pa+0.5*Pa**2)/(Pv-Pa)
+        Jloss=Kf*(0.5*sigmapi**2-sigmapi*Pv+0.5*Pv**2)/(Pv-Pa)
+        print(f"Jgain: {Jgain}")
+        print(f"Jloss: {Jloss}")
         Kblood=(1 - (1-math.exp(-P*A/F))*F/P)*(1-Rstarling)+Rstarling*Jgain/(Jgain+Jloss)
         Kisf=Rstarling*Jloss/(Jgain+Jloss)
     else:
         Kblood=1 - (1-math.exp(-P*A/F))*F/P
         Kisf=0
-    '''
-    Kblood=0.5
-    Kisf=0.5
+    #Kblood=1
+    #Kisf=1
+    print(f'Kblood: {Kblood}')
+    print(f'Kisf: {Kisf}')
     return Kblood, Kisf
     
-def CreateISFData(BloodGlucose,Times, D,T,P,F,A,Rstarling,sigmapi,Pv,Pa,Kf):
+def CreateISFData(BloodGlucose,Times, skindistanceum, D,T,P,F,A,Rstarling,sigmapi,Pv,Pa,Kf):
     #Setup
-    arraylength = 100
-    sensordistance = 95
-    array = BloodGlucose[0]*np.ones(arraylength) #Initial Value
-    SensorGlucose = np.zeros(len(BloodGlucose))
-    #constants
+    arraylength = 25
+    sensordistance = 20
+    D = D*1e12/((skindistanceum/arraylength)**2)#adjusting diffusion
+    print(f'diffusion: {D}')
+    
     bloodK, isfK = DetermineBloodISFConstant(P,F,A,Rstarling,sigmapi,Pv,Pa,Kf)
-    print(bloodK)
+    array = (BloodGlucose[0]*bloodK/(1-isfK))*np.ones(arraylength) #Initial Value
+    SensorGlucose = np.zeros(len(BloodGlucose))
     #loop
     for i in range(len(BloodGlucose)):
         SensorGlucose[i]=array[sensordistance]
         if i!=(len(BloodGlucose)-1):
-            timejump = math.floor(int(Times[i+1]-Times[i])/(T*1e9))
+            timejump = math.floor(int(Times[i+1]-Times[i])/(T*1e9))#1e9
             for j in range(timejump):
                 array=Timestep(BloodGlucose[i],array,arraylength,isfK,bloodK,T,D)
+                if i==0 and j<100:
+                    print(f'\n\nTimestep {j} for measurement {i}:')
+                    print(array)
     return SensorGlucose
 
 data = ReadData.getDataPandas(100,0)
 data= data[data['ID']==782]
 timelist = np.flip(data['time'].values)
 bloodlist = np.flip(data['glucose'].values)
-isfdata = CreateISFData(bloodlist,timelist,2e-10,0.01,15,3,1,0,25,15,35,0.978)
-print(bloodlist)
-print(isfdata)
+isfdata = CreateISFData(bloodlist,timelist,40,1e-10,0.01,15,3,1,0.2,25,15,35,0.978)#so problem is x is in meters rn, 100meters in total, so go to 1cm which is 10^4 smaller
+fig, axs = plt.subplots()
+axs.plot(timelist,bloodlist,color='red')
+axs.plot(timelist,isfdata,color='green')
+axs.legend(['Blood Glucose','Sensor Glucose'])
+axs.grid(True,which='major',linewidth='1')
+axs.grid(True,axis='x',which='minor',linewidth='0.5')
+axs.xaxis.set_major_formatter(mpld.DateFormatter('%H:%M'))
+axs.xaxis.set_minor_locator(mpld.MinuteLocator(byminute=None,interval=5,tz=None))
+plt.show()
